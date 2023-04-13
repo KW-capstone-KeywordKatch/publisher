8935089b2563f53bcb612a6b5e76f5d360a3a742
HungKungE <Jonsun59@gmail.com>
2023-04-13 22:21:03 +0900

    [send] api 요청 수정 - send요청 받으면 DB에 접근 + user_info의 모든 email에 기사 전송

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import emailSender
+import db
 
 app = Flask (__name__)
 
@@ -7,18 +8,31 @@ app = Flask (__name__)
 @app.before_first_request
 def before_first_request():
     global smtp_connect
+    global conn, cursor
     smtp_connect = emailSender.connect_smtp_server()
+    conn, cursor = db.connectionRDS()
 
 @app.route('/')
 def hello_world():
   return 'hello'
 
 # email로 기사 보내는 api
-@app.route('/send/<email>')
-def send_request(email):
+@app.route('/send')
+def send_request():
   smtp_server = smtp_connect
-  send_html = emailSender.send_email(smtp_server, email)
-  return send_html
+  user_list = db.getAllUserInfo(cursor)
 
+  # user_list : user[]
+  # user[n] : n번째 정보
+
+  # user_list -> keyword -> analyze result 접근해서 정보 가져옴
+  # 그 정보를 send_email에 보냄
+
+  for user in user_list:
+    emailSender.send_email(smtp_server, user[1])
+    print("send:"+str(user[0]))
+
+  return "end"
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port='5001', debug=True)
--- db.py
+++ db.py
@@ -1,2 +1,30 @@
-import MySQLdb
+import pymysql
+import sys
+import logging
 
+#RDS INFO
+host = "localhost" # end point
+port = 3306
+username = "root"
+database = "findvibe_db"
+password = "findvibe1"
+
+# function
+
+def connectionRDS():
+  try:
+    conn=pymysql.connect(host=host, user=username, passwd=password, db=database, port=port, use_unicode=True, charset="utf8")
+    cursor = conn.cursor()
+  except Exception as e:
+    logging.error("RDS연결 실패:"+str(e))
+    sys.exit(1)
+
+  return conn, cursor
+
+def getAllUserInfo(cursor):
+  query = "SELECT * FROM user_info"
+
+  cursor.execute(query)
+  result = cursor.fetchall()
+
+  return result
