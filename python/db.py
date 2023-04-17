502bfeb4c2d6ad48dd486e3e97d62a542a833107
HungKungE <Jonsun59@gmail.com>
2023-04-18 00:40:32 +0900

    Expand project

---
+++ python/db.py
@@ -0,0 +1,45 @@
+import pymysql
+import sys
+import logging
+import os
+ 
+# connect with RDS -----------------------------------------------------------------------
+def connectUSERRDS():
+  try:
+    conn=pymysql.connect(host=os.getenv('USER_HOST'), user=os.getenv('USER_USERNAME'), passwd=os.getenv('USER_PASSWORD'), db=os.getenv('USER_DATABASE'), port=int(os.getenv('RDS_PORT')), use_unicode=True, charset="utf8")
+    cursor = conn.cursor()
+  except Exception as e:
+    logging.error("RDS연결 실패:"+str(e))
+    sys.exit(1)
+
+  return conn, cursor
+
+def connectARTICLERDS():
+  try:
+    conn=pymysql.connect(host=os.getenv('ARTICLE_HOST'), user=os.getenv('ARTICLE_USERNAME'), passwd=os.getenv('ARTICLE_PASSWORD'), db=os.getenv('ARTICLE_DATABASE'), port=int(os.getenv('RDS_PORT')), use_unicode=True, charset="utf8")
+    cursor = conn.cursor()
+  except Exception as e:
+    logging.error("RDS연결 실패:"+str(e))
+    sys.exit(1)
+
+  return conn, cursor
+
+def connectLOCALRDS():
+  try:
+    conn=pymysql.connect(host=os.getenv('LOCAL_HOST'), user=os.getenv('LOCAL_USERNAME'), passwd=os.getenv('LOCAL_PASSWORD'), db=os.getenv('LOCAL_DATABASE'), port=int(os.getenv('RDS_PORT')), use_unicode=True, charset="utf8")
+    cursor = conn.cursor()
+  except Exception as e:
+    logging.error("RDS연결 실패:"+str(e))
+    sys.exit(1)
+
+  return conn, cursor
+
+# get user data
+
+def getAllUserInfo(cursor):
+  query = "SELECT * FROM user_info"
+
+  cursor.execute(query)
+  result = cursor.fetchall()
+
+  return result
